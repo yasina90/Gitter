bc3aec72b3fc4617a81f5d797461b68a8bbcb643
sina <haleysina@gmail.com>
2024-08-12 17:15:50 +0330

    fibonaci added

--- gitter_test.py
+++ gitter_test.py
@@ -1,2 +1,14 @@
+def count(x: list) -> int:
+    """
+    Counts the number of alphabetic characters in the given list.
 
-print("bye there!")
+    :param x: A list of characters or strings.
+    :type x: list
+    :return: The count of alphabetic characters in the list.
+    :rtype: int
+    """
+    count = 0
+    for i in x:
+        if i.isalpha():
+            count += 1
+    return count
